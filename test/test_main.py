9c6c0704ad5ce954e640c46cb6f09ac7c1dd1507
Splasky <henry860326@yahoo.com.tw>
2017-07-18 21:58:53 +0800

    Update url to test.

--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,14 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 # vim:fenc=utf-8
-# Last modified: 2017-07-18 20:33:06
+# Last modified: 2017-07-18 21:03:34
 
 from src.image_download import image_download
 import os
 
 
 def test_main():
-    article = "https://www.ptt.cc/bbs/KanColle/M.1498913943.A.63D.html"
+    article = "https://www.ptt.cc/bbs/KanColle/M.1500338012.A.8C0.html"
     directory = "/tmp/image_download/test/"
     downloader = image_download(directory=directory)
     downloader.download(article)
